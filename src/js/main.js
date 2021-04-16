a7e482cb67aa2cbbb3eaec892b723789672e9cd6
ipdes <ipdes.by@gmail.com>
2021-04-16 20:33:49 +0300

    Изменения от 16.04.2021

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 jQuery(function ($) {
-  document.getElementById("btn-menu").onclick = function (event) {
+  
+  /*document.getElementById("btn-menu").onclick = function (event) {
     document.getElementsByTagName("nav")[0].classList.toggle("expand");
-    document.getElementById("btn-menu").classList.toggle("expand");
-  };
+  };*/
+
+  $('#btn-menu').on('click', function(event){
+    $(event.currentTarget).closest('nav').toggleClass("expand");
+  });
+
 });
